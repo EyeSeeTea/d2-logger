2e9c1f4e3aa455ca304b797cafd809de695d07c4
Ana Garcia <ana@eyeseetea.com>
2024-01-24 09:06:46 +0100

    First version library structure

---
+++ scripts/publish.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e -u -o pipefail
+
+version=$(cat package.json | jq -r '.version')
+publish_opts=$(if echo "$version" | grep -q beta; then echo "--tag beta"; fi)
+
+rm build -rf
+yarn build
+yarn publish $publish_opts --new-version "$version" build/
+
+git tag "v$version" -f
+git push --tags
