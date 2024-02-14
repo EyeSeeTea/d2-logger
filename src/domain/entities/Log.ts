3a813f3c71fb90d7a1a28b80d48c49f2a389bc48
Ana Garcia <ana@eyeseetea.com>
2024-02-14 09:23:26 +0100

    Discriminating unions in Log entity adding Default and Tracker log entity

--- src/domain/entities/Log.ts
+++ src/domain/entities/Log.ts
@@ -1,6 +1,28 @@
-export type Log = {
+import { Id } from "./Base";
+
+export type Log = DefaultLog | TrackerProgramLog;
+
+export type DefaultLog = {
     message: string;
     messageType: MessageType;
 };
 
 export type MessageType = "Debug" | "Info" | "Success" | "Warn" | "Error";
+
+export type TrackerProgramLog = {
+    config: TrackerProgramMessageConfig;
+    messageType: MessageType;
+    messages: TrackerProgramMessages[];
+};
+
+export type TrackerProgramMessageConfig = {
+    trackedEntityId: Id;
+    programStageId: Id;
+    enrollmentId: Id;
+    eventStatus?: "ACTIVE" | "COMPLETED" | "VISITED" | "SCHEDULE" | "OVERDUE" | "SKIPPED";
+};
+
+export type TrackerProgramMessages = {
+    id: Id;
+    value: string;
+};
