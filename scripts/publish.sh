c2e50c7d5bd40ce2096ce611f846c223bc60b91d
Miquel Adell <miquel.adell@gmail.com>
2025-09-26 13:39:10 +0200

    mac commands need to have the modifiers after the command

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -4,7 +4,7 @@ set -e -u -o pipefail
 version=$(cat package.json | jq -r '.version')
 publish_opts=$(if echo "$version" | grep -q beta; then echo "--tag beta"; fi)
 
-rm build -rf
+rm -rf build 
 yarn build
 yarn publish $publish_opts --new-version "$version" build/
 
